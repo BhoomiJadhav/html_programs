942cb75858e9c25f0e41d3f01a74f0e853749409
Bhoomi Jadhav <jadhavbhoomi16@gmail.com>
2025-01-18 12:45:24 +0530

    js

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+console.log("Welcome to javascript");
+console.log("this is console statement");
+
+let a = 10;
+let b = 56;
+if (a > b) {
+  console.log("a is greater");
+} else {
+  console.log("b is greater");
+}
