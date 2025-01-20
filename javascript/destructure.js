9001304607f2bbaa10f86d3052088e71a4015958
Bhoomi Jadhav <jadhavbhoomi16@gmail.com>
2025-01-20 14:38:52 +0530

    update

--- javascript/destructure.js
+++ javascript/destructure.js
@@ -1,11 +1,11 @@
 const person = {
   name: "Sanket",
-  age: 19,
+  age: 23,
   email: "sanket08@gmail.com",
 };
 console.log(`person name : ${person.name}`);
 console.log(`person age : ${person.age}`);
-console.log(`person email : ${person.email}`);//st1
+console.log(`person email : ${person.email}`); //st1
 
 //adding object destructure
 const { name, age, email } = person;
